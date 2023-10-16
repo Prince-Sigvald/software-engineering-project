0d64e8ad303f46ab824a82901d13ba696fe72f9b
Yan de Hahn <jan.gridling@hotmail.com>
2023-10-16 15:04:39 +0200

    First Draft of Data Logger Class and first usage in the integration class

--- src/data_logger.py
+++ src/data_logger.py
@@ -1,7 +1,25 @@
+from datetime import datetime
+import csv
+
 class data_logger:
-    def timestamp():
-        pass
-    def csv_data():
-        pass
-    def csv_save():
-        pass
+    def __init__(self, shape, color):
+        self.shape = shape
+        self.color = color
+
+    def timestamp(self):
+        now = datetime.now()
+        self.date = now.strftime("%Y-%m-%d %H:%M:%S")
+
+    def csv_save(self, file_path):
+        # Überprüfe, ob die Datei existiert. Falls nicht, lege sie an und schreibe die Überschriften.
+        try:
+            with open(file_path, mode='x', newline='') as csv_file:
+                csv_writer = csv.writer(csv_file, delimiter=',')
+                csv_writer.writerow(['Datum/Uhrzeit', 'Form', 'Farbe'])
+        except FileExistsError:
+            pass
+
+        # Füge die aktuellen Daten hinzu, jedes Attribut in einer eigenen Spalte
+        with open(file_path, mode='a', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file, delimiter=',')
+            csv_writer.writerow([self.date, self.shape, self.color])
